648b7de645cbec338761f30e0240ed4285365e5b
Demiana Melek <132511157+demianamelek1@users.noreply.github.com>
2024-12-11 00:24:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+// (divideby10)
+
+// function divide(x){
+//     x = +prompt("enter a number")
+//     if((x%10)==0) return true
+//     else return false
+// }
+// console.log(divide())
+
+//////////////////////////////////////////////////////////////////
+
+//max num in array 
+
+// let numbers = [10, 20, 150, 40, 200,500];
+// function max(arr){
+//     let max = arr[0]
+//     for (i in arr){
+//         if(arr[i]>max){
+//             max = arr[i]
+//         }
+//     }
+//     return max
+// }
+// console.log(max(numbers))
+
+//////////////////////////////////////////////////////////////////
+
+// reverse array
+
+// let numbers = [10, 20, 30, 40, 50, 60];
+
+// function rev(arr) {
+//     let reverse = [];
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//         reverse.push(arr[i]);
+//         }
+//     return reverse;
+// }
+// console.log(rev(numbers)); 
+
+
+
